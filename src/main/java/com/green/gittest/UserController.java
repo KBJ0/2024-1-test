71be3f1035af12595d0cb6f2dfd97df0a61d9725
boss75045 <soochan1201@gmail.com>
2024-05-23 15:54:59 +0900

    어노테이션 추가

--- src/main/java/com/green/gittest/UserController.java
+++ src/main/java/com/green/gittest/UserController.java
@@ -1,8 +1,14 @@
 package com.green.gittest;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequiredArgsConstructor
+@RequestMapping
 public class UserController {
 
     // KBJ 테스트
