839a595980ca89ba2ff773dbcb67ece693860976
hobbang7531 <songyerim1@naver.com>
2024-06-12 14:32:17 +0900

    0612 email 부분 수정

--- src/main/java/com/green/gittest/email/MailController.java
+++ src/main/java/com/green/gittest/email/MailController.java
@@ -21,43 +21,36 @@ import javax.mail.internet.AddressException;
 public class MailController {
     private final MailSendService mailService;
 
-    @PostMapping ("/send")
-    @Operation(summary = "인증 이메일 전송" , description = "이메일 인증할 이메일을 입력합니다")
-    public MailResult<MailSendRes> mailSend(@RequestBody @Valid EmailRequestDto emailDto) throws AddressException  {
-
-        try {
-            System.out.println("이메일 인증 요청이 들어옴");
-            System.out.println("이메일 인증 이메일 :" + emailDto.getEmail());
-           MailSendRes result = mailService.joinEmail(emailDto.getEmail());
-            if(result.getEmailCheckCode().equals("이미 가입된 이메일입니다.")){
-                return MailResult.<MailSendRes>builder()
-                        .code("DE")
-                        .message("이미 가입된 이메일입니다.")
-                        .data(null).build();
-            }
+    @PostMapping("/send")
+    @Operation(summary = "인증 이메일 전송", description = "이메일 인증할 이메일을 입력합니다")
+    public MailResult<MailSendRes> mailSend(@RequestBody @Valid EmailRequestDto emailDto) throws AddressException {
+        System.out.println("이메일 인증 요청이 들어옴");
+        System.out.println("이메일 인증 이메일 :" + emailDto.getEmail());
+        MailSendRes result = mailService.joinEmail(emailDto.getEmail());
+        if (result.getEmailCheckCode().equals("이미 가입된 이메일입니다.")) {
             return MailResult.<MailSendRes>builder()
-                    .code("SU")
-                    .message("인증번호를 보냈습니다")
-                    .data(result).build();
-
-        } catch (Exception e){
-            throw new AddressException();
+                    .code("DE")
+                    .message("이미 가입된 이메일입니다.")
+                    .data(null).build();
         }
+        return MailResult.<MailSendRes>builder()
+                .code("SU")
+                .message("인증번호를 보냈습니다")
+                .data(result).build();
     }
 
     @PostMapping("/auth_check")
-    @Operation(summary = "이메일 인증 확인" , description = "받은 인증번호를 입력합니다")
-    public MailResult<MailCheckRes> AuthCheck(@RequestBody @Valid EmailCheckDto emailCheckDto){
-        Boolean Checked= mailService.CheckAuthNum(emailCheckDto.getEmail(),emailCheckDto.getAuthNum());
+    @Operation(summary = "이메일 인증 확인", description = "받은 인증번호를 입력합니다")
+    public MailResult<MailCheckRes> AuthCheck(@RequestBody @Valid EmailCheckDto emailCheckDto) {
+        Boolean Checked = mailService.CheckAuthNum(emailCheckDto.getEmail(), emailCheckDto.getAuthNum());
         MailCheckRes res = new MailCheckRes();
-        if(Checked){
+        if (Checked) {
             res.setEmailChecked(true);
             return MailResult.<MailCheckRes>builder()
                     .code("SU")
                     .message("인증이 완료되었습니다.")
                     .data(res).build();
-        }
-        else{
+        } else {
             res.setEmailChecked(false);
             return MailResult.<MailCheckRes>builder()
                     .code("IC")
